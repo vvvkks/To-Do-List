1520cee9ecdee89336a9653c69e4d3ce97abe319
vvvkks <kurilova2003@gmail.com>
2023-10-25 22:08:44 +0300

    add: tests for todolist reducer

---
+++ src/state/todolists-reducer.test.ts
@@ -0,0 +1,42 @@
+import {todolistsReducer} from "./todolists-reducer";
+import {TodolistType} from "../App";
+import {v1} from 'uuid'
+
+test('correct todolist should be removed', () => {
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: "My tasks", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"},
+    ]
+
+    const endState = todolistsReducer(startState, {
+        type: 'REMOVE-TODOLIST',
+        id: todolistId1
+    })
+
+    expect(endState.length).toBe(1);
+    expect(endState[0].id).toBe(todolistId2);
+})
+
+test('correct todolist should be added', () => {
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
+    let newTodolistTitle = "New Todolist";
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: "My tasks", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"},
+    ]
+
+    const endState = todolistsReducer(startState, {
+        type: 'ADD-TODOLIST',
+        title: newTodolistTitle
+    })
+
+    expect(endState.length).toBe(3);
+    expect(endState[2].title).toBe(newTodolistTitle)
+    expect(endState[2].filter).toBe("all")
+})
