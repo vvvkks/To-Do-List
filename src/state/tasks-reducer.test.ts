82402c41634a2e30b927f3b7119afd34753a4ad2
vvvkks <kurilova2003@gmail.com>
2023-11-05 17:52:35 +0300

    test: changing tasks status

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,5 +1,5 @@
 import {TasksStateType} from "../App";
-import {addTaskAC, removeTaskAC, tasksReducer} from "./tasks-reducer";
+import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer} from "./tasks-reducer";
 
 test('correct task should be deleted from correct array', () => {
     const startState: TasksStateType = {
@@ -48,4 +48,26 @@ test('correct task should be added from correct array', () => {
     expect(endState["todolistId2"][0].id).toBeDefined();
     expect(endState["todolistId2"][0].title).toBe("juce");
     expect(endState["todolistId2"][0].isDone).toBe(false);
+});
+
+test('status of specified task should be changed', () => {
+    const startState: TasksStateType = {
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false},
+            { id: "2", title: "JS", isDone: true},
+            { id: "3", title: "React", isDone: false}
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false},
+            { id: "2", title: "milk", isDone: true},
+            { id: "3", title: "tea", isDone: false}
+        ]
+    };
+
+    const action = changeTaskStatusAC("2", false,  "todolistId2");
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState["todolistId2"][1].isDone).toBeFalsy();
+    expect(endState["todolistId1"][1].isDone).toBeTruthy();
 });
