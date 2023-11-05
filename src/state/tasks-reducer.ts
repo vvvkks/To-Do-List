c8b41f7c0300142426c744de8915290b093577bf
vvvkks <kurilova2003@gmail.com>
2023-11-05 18:00:49 +0300

    add: tasks reducer

---
+++ src/state/tasks-reducer.ts
@@ -0,0 +1,95 @@
+import {TasksStateType} from '../App'
+import {v1} from "uuid";
+
+export type RemoveTaskActionType = {
+    type: 'REMOVE-TASK',
+    todolistId: string,
+    taskId: string
+}
+
+export type AddTaskActionType = {
+    type: 'ADD-TASK',
+    todolistId: string,
+    todolistTitle: string,
+}
+
+export type ChangeTaskStatusActionType = {
+    type: 'CHANGE-TASK-STATUS',
+    taskId: string,
+    todolistId: string,
+    isDone: boolean
+}
+
+export type ChangeTaskTitleActionType = {
+    type: 'CHANGE-TASK-TITLE',
+    taskId: string,
+    todolistId: string,
+    title: string
+}
+
+
+export type ActionsType = RemoveTaskActionType | AddTaskActionType | ChangeTaskStatusActionType | ChangeTaskTitleActionType
+
+export const tasksReducer = (state: TasksStateType, action: ActionsType):  TasksStateType  => {
+    switch (action.type) {
+        case 'REMOVE-TASK': {
+            const stateCopy = {...state}
+            const tasks = state[action.todolistId];
+            const filteredTasks = tasks.filter(t => t.id !== action.taskId);
+
+            stateCopy[action.todolistId] = filteredTasks;
+
+            return stateCopy;
+        }
+        case 'ADD-TASK': {
+            const stateCopy = {...state};
+            const tasks = stateCopy[action.todolistId];
+            const newTask = { id: v1(), title: action.todolistTitle, isDone: false,};
+            const newTasks = [newTask, ...tasks];
+
+            stateCopy[action.todolistId] = newTasks;
+
+            return stateCopy;
+        }
+        case 'CHANGE-TASK-STATUS': {
+            const stateCopy = {...state};
+            let tasks = state[action.todolistId];
+            let task = tasks.find(t => t.id === action.taskId);
+
+            if (task) {
+                task.isDone = action.isDone;
+            }
+
+            return stateCopy;
+        }
+        case 'CHANGE-TASK-TITLE': {
+            const stateCopy = {...state};
+            let tasks = state[action.todolistId];
+            let task = tasks.find(t => t.id === action.taskId);
+
+            if (task) {
+                task.title = action.title;
+            }
+
+            return stateCopy;
+        }
+        default:
+            throw new Error("Unknown action type");
+    }
+}
+
+export const removeTaskAC = (taskId: string, todolistId: string): RemoveTaskActionType => {
+    return {type: "REMOVE-TASK", todolistId: todolistId, taskId: taskId}
+}
+
+export const addTaskAC = (todolistTitle: string, todolistId: string): AddTaskActionType => {
+    return {type: "ADD-TASK", todolistTitle, todolistId}
+}
+
+export const changeTaskStatusAC = (taskId: string, isDone: boolean, todolistId: string): ChangeTaskStatusActionType => {
+    return {type: "CHANGE-TASK-STATUS", isDone, todolistId, taskId}
+}
+
+export const changeTaskTitleAC = (taskId: string, title: string, todolistId: string): ChangeTaskTitleActionType => {
+    return {type: "CHANGE-TASK-TITLE", title, todolistId, taskId}
+}
