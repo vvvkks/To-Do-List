eb0d804a042df174d4f2f6549f9924e4603f0dd1
vvvkks <kurilova2003@gmail.com>
2024-02-05 20:43:24 +0300

    refactor: CHANGE-TASK-STATUS case and CHANGE-TASK-TITLE case

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -55,23 +55,20 @@ export const tasksReducer = (state: TasksStateType =initialState, action: Action
             return stateCopy;
         }
         case 'CHANGE-TASK-STATUS': {
-            const stateCopy = {...state};
-
-            let tasks = state[action.todolistId];
-            stateCopy[action.todolistId] = tasks.map(t => t.id === action.taskId ?  {...t, isDone: action.isDone} : t)
-
-            return stateCopy;
+            let todolistTasks = state[action.todolistId];
+            state[action.todolistId] = todolistTasks
+                .map(t => t.id === action.taskId
+                    ? {...t, isDone: action.isDone}
+                    : t);
+            return ({...state});
         }
         case 'CHANGE-TASK-TITLE': {
-            const stateCopy = {...state};
-            let tasks = state[action.todolistId];
-            let task = tasks.find(t => t.id === action.taskId);
-
-            if (task) {
-                task.title = action.title;
-            }
-
-            return stateCopy;
+            let todolistTasks = state[action.todolistId];
+            state[action.todolistId] = todolistTasks
+                .map(t => t.id === action.taskId
+                    ? {...t, title: action.title}
+                    : t);
+            return ({...state});
         }
         case 'ADD-TODOLIST': {
             const stateCopy = {...state};
